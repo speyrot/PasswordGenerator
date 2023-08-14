820037985f3d94346b062f1b40564df022c2adf8
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-08-14 10:53:07 -0400

    wrote random functions

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
 
+function getRandomLower () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber () {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol () {
+    const symbols = '!@#$%^&*(){}[]+<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
